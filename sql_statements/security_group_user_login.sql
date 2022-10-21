2d81ec07f834f3570d0f58ab906ca97a97e583e9
huyvhd293 <duchuy.vohuynh@gmail.com>
2022-10-21 15:00:59 +0700

    Update security_group_user_login.sql

--- sql_statements/security_group_user_login.sql
+++ sql_statements/security_group_user_login.sql
@@ -13,3 +13,26 @@ on (s.principal_id = l.principal_id)
 -- R = SERVER_ROLE
 WHERE s.type NOT IN ('C', 'R')
 ORDER BY s.name;
+
+-- List database permission for each user
+SELECT
+    perms.state_desc AS State,
+    permission_name AS [Permission],
+    obj.name AS [on Object],
+    dp.name AS [to User Name]
+FROM sys.database_permissions AS perms
+JOIN sys.database_principals AS dp
+    ON perms.grantee_principal_id = dp.principal_id
+JOIN sys.objects AS obj
+    ON perms.major_id = obj.object_id;
+
+-- List server-role members
+SELECT roles.principal_id AS RolePrincipalID,
+    roles.name AS RolePrincipalName,
+    server_role_members.member_principal_id AS MemberPrincipalID,
+    members.name AS MemberPrincipalName
+FROM sys.server_role_members AS server_role_members
+INNER JOIN sys.server_principals AS roles
+    ON server_role_members.role_principal_id = roles.principal_id
+LEFT JOIN sys.server_principals AS members
+    ON server_role_members.member_principal_id = members.principal_id;
