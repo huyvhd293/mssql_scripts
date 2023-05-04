e38f205bfb8e5aa631096f3b106694f6350ff57c
huyvhd293 <duchuy.vohuynh@gmail.com>
2023-05-04 17:21:26 +0700

    Update new statement about drop/recreate constraint

---
+++ sql_statements/generate_drop_all_constraint.sql
@@ -0,0 +1,8 @@
+-- Generate sql statement for dropping all constraint in a database
+SELECT N'ALTER TABLE ' + QUOTENAME(cs.name) + '.' + QUOTENAME(ct.name) 
+    + ' DROP CONSTRAINT ' + QUOTENAME(fk.name) + ';'
+FROM sys.foreign_keys AS fk
+INNER JOIN sys.tables AS ct
+  ON fk.parent_object_id = ct.[object_id]
+INNER JOIN sys.schemas AS cs 
+  ON ct.[schema_id] = cs.[schema_id];
